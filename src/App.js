59696cda92291d2f179500905aac28285ea0ead4
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-06-09 12:51:08 +0200

    add pages and react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Nav from "./templates/Nav";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Archive from "./pages/Archive";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </div>
   );
 }
