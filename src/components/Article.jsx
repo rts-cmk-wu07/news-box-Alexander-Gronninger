a083740e5d3bced837135c3c052421b300768170
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-06-14 14:14:05 +0200

    Create Article.jsx

---
+++ src/components/Article.jsx
@@ -0,0 +1,18 @@
+const Article = () => {
+  return (
+    <>
+      <article>
+        <img src="" alt="" />
+        <h2>Test Article</h2>
+        <p>
+          Dolore exercitation velit voluptate tempor. Consequat laboris nulla
+          dolor magna in dolor pariatur ex. Mollit tempor consectetur ut magna
+          ea enim. Et ea mollit proident occaecat consequat incididunt ut
+          excepteur ipsum.
+        </p>
+      </article>
+    </>
+  );
+};
+
+export default Article;
