cb21172c17612b5a28ef149572a042556bfec4c3
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-06-14 21:24:00 +0200

    style for articleCategories

--- src/components/ArticleCategories.jsx
+++ src/components/ArticleCategories.jsx
@@ -1,4 +1,5 @@
 import { IoIosArrowBack } from "react-icons/io";
+import { BiCategory } from "react-icons/bi";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
@@ -6,11 +7,38 @@ const ArticleCategories = (props) => {
   const styles = {
     category: css`
       display: flex;
+      align-items: center;
+      height: 60px;
+      border-bottom: 1px solid lightgray;
+      & div {
+        margin: 0 0 0 10px;
+        border-radius: 50%;
+        width: 35px;
+        height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-shadow: 0px 7px 20px 5px lightgray;
+        & svg {
+          margin: 0;
+        }
+      }
+      & h2 {
+        margin: 0 auto 0 5px;
+        line-height: 24px;
+      }
+      & svg {
+        height: 24px;
+        width: auto;
+        margin: 0 15px 0 0;
+      }
     `,
   };
   return (
     <section css={styles.category}>
-      <div></div>
+      <div>
+        <BiCategory />
+      </div>
       <h2>{props.category}</h2>
       <IoIosArrowBack />
     </section>
