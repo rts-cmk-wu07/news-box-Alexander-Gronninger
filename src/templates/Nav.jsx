1a888c424cacc427683f9e893dd050934421c0be
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-06-14 14:25:23 +0200

    style nav

--- src/templates/Nav.jsx
+++ src/templates/Nav.jsx
@@ -2,15 +2,30 @@ import ArchiveIcon from "../components/ArchiveIcon";
 import ArrowIcon from "../components/ArrowIcon";
 import SettingIcon from "../components/SettingIcon";
 import { useLocation } from "react-router-dom";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 const pageTitle = "";
 
 const Nav = () => {
+  const styles = {
+    nav: css`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 20px;
+      border-bottom: 1px solid lightgray;
+      & svg {
+        height: 24px;
+        width: auto;
+      }
+    `,
+  };
   let pageURL = useLocation().pathname;
   return (
     <>
       <header>
-        <nav>
+        <nav css={styles.nav}>
           {(useLocation().pathname === "/" && <ArchiveIcon />) || <ArrowIcon />}
           <h1>
             {(pageURL === "/" && "Newsbox") ||
